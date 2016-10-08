59039ea3bbba4f914dae43b866eff9e72eee964c
Ravi Sojitra <ravisojitra79@gmail.com>
2016-10-08 20:12:41 +0530

    MEAN-CRUD
    This ongoing application is demo of CRUD developed in MEAN stack +
    MaterializeCSS

---
+++ app/signup/signup-controller.js
@@ -0,0 +1,11 @@
+(function(){
+    angular.module('TimeWaste').controller('SignupController',['$scope','$state','$http',function($scope,$state,$http){
+        $scope.createUser = function(){
+            $http.post('api/user/signup',$scope.newUser).success(function(response){
+                
+            }).error(function(error){
+                console.log(error);
+            })
+        }
+    }]);
+}());
---
+++ server/controllers/authentication-controller.js
@@ -0,0 +1,9 @@
+
+var User = require('../datasets/users');
+
+module.exports.signup = function(req,res){
+    var user = new User(req.body);
+    user.save();
+    
+    res.json(req.body);
+}
